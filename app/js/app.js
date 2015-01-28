e0b2e5e15afb9b6596f09abc7facfa8871532835
Martin Turjak <martinturjak@gmail.com>
2015-01-27 23:44:11 -0800

    top navigation bar

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,23 @@
-angular.module('app', []);
+angular.module('app', ['ui.bootstrap'])
+
+.controller('AppCtrl', function() {
+	this.appName = 'My App';
+})
+
+.controller('NavBarCtrl', ['$scope', '$location', '$window', function($scope, $location, $window) {
+	$scope.isCollapsed = false;
+
+	$scope.doCollapse = function() {
+		if($($window).innerWidth() < 768) {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		}
+	};
+
+	$scope.isActive = function(path) {
+	    if ($location.path() === path) {
+	      return "active";
+	    } else {
+	      return "";
+	    }
+	};
+}]);
